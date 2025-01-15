e86e756c6b07a87863a736b927baaa93924e82df
JMSCompSci <twright@jnpsd.org>
2025-01-15 12:54:52 -0600

    Added DOSBox support!

---
+++ Emus/DOS/launch_db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#echo "===================================="
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/DOS
+cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
+$EMU_DIR/cpuswitch.sh
+
+#disable netplay
+NET_PARAM=
+
+#HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/dosbox_pure_libretro.so "$*"
+
+#HOME=$RA_DIR/ $RA_DIR/retroarch -v $NET_PARAM -L $RA_DIR/.retroarch/cores/dosbox_core_libretro.so "$*"
+HOME=$RA_DIR/ $RA_DIR/retroarch -v $NET_PARAM -L $RA_DIR/.retroarch/cores/dosbox_libretro.so "$*"
+#HOME=$RA_DIR/ $RA_DIR/retroarch -v $NET_PARAM -L $EMU_DIR/libmgba.so "$*"
