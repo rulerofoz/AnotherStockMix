891e4ae2868f2e22f20e9cc0ae472af72f7bfcfa
RulerOfOZ <ackliph@gmail.com>
2025-02-06 12:23:49 -0600

    PPSSPP bug
    The default version doesn't work.  So i switch the default to vulkan.

--- Emus/PPSSPP/launch.sh
+++ Emus/PPSSPP/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#set > /tmp/env.txt
 echo $0 $*
 progdir=`dirname "$0"`
-cd $progdir
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$progdir
+progdir181_vulkan=$progdir/PPSSPP_1.18.1_vulkan
+cd $progdir181_vulkan
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$progdir181_vulkan
 
 echo "=============================================="
 echo "==================== PPSSPP  ================="
@@ -12,16 +12,6 @@ echo "=============================================="
 ./cpufreq.sh
 ./cpuswitch.sh
 
-export HOME=/mnt/SDCARD
-export SDL_GAMECONTROLLERCONFIG_FILE=/mnt/SDCARD/Emus/PPSSPP/assets/gamecontrollerdb.txt
-#export SDL_AUDIODRIVER=dsp   //disable 20231031 for sound suspend issue
 
-
-export SDL_GAMECONTROLLERCONFIG_FILE=/mnt/SDCARD/Emus/PPSSPP/assets/gamecontrollerdb.txt
-#export PATH=/usr/sbin:/usr/bin:/sbin:/bin
-#export SHLVL=2
-export LD_LIBRARY_PATH=./:/mnt/SDCARD:/mnt/SDCARD/lib:/mnt/UDISK:/usr/trimui/lib/:/usr/miyoo/lib:/customer/lib/:/config/lib/:/lib:/usr/lib::/mnt/SDCARD/Emus/PPSSPP
-export OLDPWD=/mnt/SDCARD/Emus/PPSSPP
-set > /tmp/env.txt
-./PPSSPPSDL_gl "$*"
-
+export HOME=$progdir181_vulkan
+./PPSSPPSDL_vulkan "$*"
