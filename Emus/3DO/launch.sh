c4a11e475d61055b4a844948afd41ccbc9c4ffef
RulerOfOZ <ackliph@gmail.com>
2025-02-09 19:56:01 -0600

    added 3do support
    and also fixed snes name.

---
+++ Emus/3DO/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/3DO
+cd $RA_DIR/
+
+$EMU_DIR/cpufreq.sh
+$EMU_DIR/cpuswitch.sh
+
+#disable netplay
+NET_PARAM=
+
+HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v $NET_PARAM -L $RA_DIR/.retroarch/cores/opera_libretro.so "$*"
